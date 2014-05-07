e4e926d57d4bf8e93fc2d7166c270593248064e5
jimmgarrido <jimmgarrido@gmail.com>
2014-05-06 19:28:00 -0700

    Added connection to Azure

--- AzureStorage/MainWindow.xaml.cs
+++ AzureStorage/MainWindow.xaml.cs
@@ -13,19 +13,82 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
 
-namespace WpfApplication1
+namespace AzureStorage
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        string[] containers = { "photos", "videos", "files", "test" };
+        private string account, key;
+        CloudStorageAccount storageAccount;
+         
 
         public MainWindow()
         {
             InitializeComponent();
+            
+            try
+            {
+                storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"].ToString());
+                DisableForm();
+                OpenAccount();
+            } 
+            catch
+            {
+            }
+          
+
+        }
+
+        private void login_Click_1(object sender, RoutedEventArgs e)
+        {
+            account = accountBox.Text;
+            key = keyBox.Text;
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings.Add("user", account);
+            config.AppSettings.Settings.Add("StorageConnectionString", "DefaultEndpointsProtocol=https;AccountName=jimmygarrido;AccountKey=tPdoyMB5YopNibic+IxnzzzkYZlIhAa3ojS1gCFY/SRuq4NYcGFN6oypH5SjvPdtgUrICA4tFsMF9w9pSRjBJQ==");
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+
+            try
+            {
+                storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"].ToString());
+                DisableForm();
+            }
+            catch
+            {
+            }
+
+            OpenAccount();
+        }
+
+        private void OpenAccount()
+        {
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            List<CloudBlobContainer> containerNames = new List<CloudBlobContainer>();
+
+            foreach(var container in blobClient.ListContainers())
+            {
+                containerNames.Add(container);
+            }
+
+            containersList.ItemsSource = containerNames;
+
+        }
+
+        private void DisableForm()
+        {
+            accountBox.Text = ConfigurationManager.AppSettings["user"].ToString();
+            accountBox.IsEnabled = false;
+            keyBox.IsEnabled = false;
         }
     }
 }
